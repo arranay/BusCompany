65ae4511fed70e953f274cea092e73414c387b18
Валерия Хлызова <valeriakhlyzoba@gmail.com>
2019-11-11 23:54:08 +0300

    Добавлена модель для работы с водителями

---
+++ BusCompany/Models/Driver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusCompany.Models
+{
+    public class Driver : Employees
+    {
+        private string categories;
+        private string rightsDate;
+        private bool onRoute;
+
+        public string Categories { get => categories; set => categories = value; }
+        public string RightsDate { get => rightsDate; set => rightsDate = value; }
+        public bool OnRoute { get => onRoute; set => onRoute = value; }
+    }
+}
--- BusCompany/Models/Employees.cs
+++ BusCompany/Models/Employees.cs
@@ -7,20 +7,20 @@ namespace BusCompany.Models
 {
     public class Employees
     {
-        private int personnelNumber;
-        private string lastName;
-        private string firstName;
-        private string middleName;
-        private String dateOfBirth;
-        private int experience;
-        private decimal salary;
-        private string position;
+        protected int personnelNumber;
+        protected string lastName;
+        protected string firstName;
+        protected string middleName;
+        protected string dateOfBirth;
+        protected int experience;
+        protected decimal salary;
+        protected string position;
 
         public int PersonnelNumber { get => personnelNumber; set => personnelNumber = value; }
         public string LastName { get => lastName; set => lastName = value; }
         public string FirstName { get => firstName; set => firstName = value; }
         public string MiddleName { get => middleName; set => middleName = value; }
-        public String DateOfBirth { get => dateOfBirth; set => dateOfBirth = value; }
+        public string DateOfBirth { get => dateOfBirth; set => dateOfBirth = value; }
         public int Experience { get => experience; set => experience = value; }
         public decimal Salary { get => salary; set => salary = value; }
         public string Position { get => position; set => position = value; }

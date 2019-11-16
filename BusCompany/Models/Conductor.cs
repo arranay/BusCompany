c5b26195df2b1aeabbd2945e5d48ca5055ff72f9
Валерия Хлызова <valeriakhlyzoba@gmail.com>
2019-11-16 18:48:46 +0300

    созданы модели для сущностей Кондуктор и Механик

---
+++ BusCompany/Models/Conductor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusCompany.Models
+{
+    public class Conductor : Employees
+    {
+        private bool onRoute;
+        public bool OnRoute { get => onRoute; set => onRoute = value; }
+    }
+}
---
+++ BusCompany/Models/Mechanic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusCompany.Models
+{
+    public class Mechanic
+    {
+        private string qualification;
+        public string Qualification { get => qualification; set => qualification = value; }
+    }
+}

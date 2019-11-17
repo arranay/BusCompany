73914daed7dfd6e196157bca55d9116276d67e78
Валерия Хлызова <valeriakhlyzoba@gmail.com>
2019-11-17 13:43:54 +0300

    Добавлены страницы вывода всех автобусов и информации о них

---
+++ BusCompany/Controllers/BusController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusCompany.DAO;
+using BusCompany.Models;
+
+namespace BusCompany.Controllers
+{
+    public class BusController : Controller
+    {
+        BusDAO busDAO = new BusDAO();
+
+        // GET: Bus
+        public ActionResult Index()
+        {
+            return View(busDAO.GetAll());
+        }
+
+        // GET: Bus/Details/5
+        public ActionResult Details(string id)
+        {
+            return View(busDAO.GetById(id));
+        }
+
+        // GET: Bus/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Bus/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Bus/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Bus/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Bus/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Bus/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BusCompany/DAO/BusDAO.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using BusCompany.Models;
+
+namespace BusCompany.DAO
+{
+    public class BusDAO : ClassDAO
+    {
+        public List<Bus> GetAll()
+        {
+            Connect();
+            log4net.Config.DOMConfigurator.Configure();
+            log.Info("Вызывается метод который возвращает список всех автобусов.");
+
+            List<Bus> busList = new List<Bus>();
+            SqlCommand commandRead = new SqlCommand("SELECT*FROM Bus;", Connection);
+            SqlDataReader reader = commandRead.ExecuteReader();
+            try
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Bus bus = new Bus();
+                        bus.NumberPlate = Convert.ToString(reader["NumberPlate"]);
+                        bus.Mark = Convert.ToString(reader["Mark"]);
+                        bus.TechnicalStatus = Convert.ToBoolean(reader["TechnicalStatus"]);
+                        bus.Speed = Convert.ToInt32(reader["Speed"]);
+                        bus.FuelConsumption = Convert.ToInt32(reader["FuelConsumption"]);
+                        bus.Mileage = Convert.ToInt32(reader["Mileage"]);
+                        bus.Status = Convert.ToBoolean(reader["Status"]);
+                        busList.Add(bus);
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                log.Error("ERROR" + e.Message);
+            }
+            finally
+            {
+                reader.Close();
+                Disconnect();
+            }
+
+            return busList;
+        }
+
+        public Bus GetById(string id)
+        {
+            Connect(); Bus bus = new Bus();
+            string query = "SELECT*FROM Bus where numberPlate='" + id+"'";
+            SqlCommand commandRead = new SqlCommand(query, Connection);
+            SqlDataReader reader = commandRead.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    bus.NumberPlate = Convert.ToString(reader["NumberPlate"]);
+                    bus.Mark = Convert.ToString(reader["Mark"]);
+                    bus.TechnicalStatus = Convert.ToBoolean(reader["TechnicalStatus"]);
+                    bus.Speed = Convert.ToInt32(reader["Speed"]);
+                    bus.FuelConsumption = Convert.ToInt32(reader["FuelConsumption"]);
+                    bus.Mileage = Convert.ToInt32(reader["Mileage"]);
+                    bus.Status = Convert.ToBoolean(reader["Status"]);
+                }
+            }
+            reader.Close();
+            Disconnect();
+
+            return bus;
+        }
+
+    }
+}

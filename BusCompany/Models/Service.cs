45e2712d3945b6744997a9d3f175dc02aad99a9b
Валерия Хлызова <valeriakhlyzoba@gmail.com>
2019-11-20 19:31:04 +0300

    Добавлена модель для сущности Сервис

---
+++ BusCompany/Models/Service.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BusCompany.Models
+{
+    public class Service
+    {
+        private int id;
+        private string idBus;
+        private int idEmployees;
+        private DateTime date;
+        private string typeOfWork;
+
+        public int Id { get => id; set => id = value; }
+        public string IdBus { get => idBus; set => idBus = value; }
+        public int IdEmployees { get => idEmployees; set => idEmployees = value; }
+        [DataType(DataType.Date)]
+        public DateTime Date { get => date; set => date = value; }
+        public string TypeOfWork { get => typeOfWork; set => typeOfWork = value; }
+    }
+}

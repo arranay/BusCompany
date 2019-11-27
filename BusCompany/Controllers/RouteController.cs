a193cf0f482da42fa540055f341182660bf6fa3d
Валерия Хлызова <valeriakhlyzoba@gmail.com>
2019-11-27 17:41:24 +0300

    Вывод списка всех маршрутов

---
+++ BusCompany/Controllers/RouteController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusCompany.DAO;
+
+namespace BusCompany.Controllers
+{
+    public class RouteController : Controller
+    {
+        RouteDAO routeDAO = new RouteDAO();
+
+        // GET: Route
+        public ActionResult Index()
+        {
+            return View(routeDAO.GetAllRoute());
+        }
+
+        // GET: Route/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Route/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Route/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Route/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Route/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Route/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Route/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BusCompany/DAO/RouteDAO.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using BusCompany.Models;
+
+namespace BusCompany.DAO
+{
+    public class RouteDAO : ClassDAO
+    {
+        public List<Route> GetAllRoute()
+        {
+            Connect();
+            log4net.Config.DOMConfigurator.Configure();
+
+            log.Info("Вызывается метод который возвращает список всех маршрутов.");
+
+            List<Route> routelList = new List<Route>();
+
+            SqlCommand commandRead = new SqlCommand("SELECT*FROM Route;", Connection);
+            SqlDataReader reader = commandRead.ExecuteReader();
+            try
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Route route = new Route();
+                        route.Id = Convert.ToInt32(reader["id"]);
+                        route.RouteName = Convert.ToString(reader["RouteName"]);
+                        route.NumberOfHult = Convert.ToInt32(reader["NumberOfHult"]);
+                        route.ApprovedStatus = Convert.ToBoolean(reader["ApprovedStatus"]);
+                        routelList.Add(route);
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                log.Error("ERROR" + e.Message);
+            }
+            finally
+            {
+                reader.Close();
+                Disconnect();
+            }
+
+            return routelList;
+        }
+
+    }
+}

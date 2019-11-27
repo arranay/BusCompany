bea7bbe3fa318c5fd78b77759355bff972f8ede7
Валерия Хлызова <valeriakhlyzoba@gmail.com>
2019-11-27 17:09:20 +0300

    Добавлена модель сущности Маршрут

---
+++ BusCompany/Models/Route.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusCompany.Models
+{
+    public class Route
+    {
+        private int id;
+        private string routeName;
+        private int numberOfHult;
+        private bool approvedStatus;
+
+        List<Hult> hultOnRoute;
+
+        public int Id { get => id; set => id = value; }
+        public string RouteName { get => routeName; set => routeName = value; }
+        public int NumberOfHult { get => numberOfHult; set => numberOfHult = value; }
+        public bool ApprovedStatus { get => approvedStatus; set => approvedStatus = value; }
+        public List<Hult> HultOnRoute1 { get => hultOnRoute; set => hultOnRoute = value; }
+    }
+}

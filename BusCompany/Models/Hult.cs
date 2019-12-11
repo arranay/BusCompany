b5d8a7a51de8879aa16313733643ba1bc370650c
Валерия Хлызова <valeriakhlyzoba@gmail.com>
2019-12-11 21:26:41 +0300

    Проведено редакторование всех представлений

--- BusCompany/Models/Hult.cs
+++ BusCompany/Models/Hult.cs
@@ -11,7 +11,7 @@ namespace BusCompany.Models
         public int Id { get { return id; } set { id = value; } }
 
         [Required(ErrorMessage = "Данное поле является обязательным")]
-        [RegularExpression(@"^[A-ЯЁ][а-яё]+\s+",
+        [RegularExpression(@"^[A-ЯЁ][а-яё]+\s*([A-ЯЁ]*[а-яё]+\s*)*",
             ErrorMessage = "Название остановки должно начинатся с заглавной буквы")]
         public string HultName { get { return hultName; } set { hultName = value; } }
         public int RoutNumber { get => routNumber; set => routNumber = value; }
